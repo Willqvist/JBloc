ede77ad8cd89e374a267a7ad58c2ef53fddbb75c
Willqvist <willqvist@gmail.com>
2020-02-12 21:27:35 +0100

    changes

--- src/settings/Settings.java
+++ src/settings/Settings.java
@@ -4,5 +4,5 @@ public class Settings {
 
     public static final int SHORT = 16, NORMAL = 32, LONG = 64;
 
-    public static int renderDistance = 5;
+    public static int renderDistance = SHORT;
 }
